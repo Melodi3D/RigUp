ae4923bd04378f645d17d14a0beddcf306890a9b
Melodi <83541357+Melodi3D@users.noreply.github.com>
2026-09-23 18:49:22 -0400

    Update RigUp.py

--- RigUp.py
+++ RigUp.py
@@ -1,5 +1,6 @@
 """ RigUp by Melodi """
 """ Copyright 2026 """
+
 import maya.cmds as cmds
 import maya.mel as mel
 import maya.api.OpenMaya as om
@@ -10,212 +11,295 @@ from functools import partial
 import sys
 import os
 
+
 ########################################################################################################################
 # Guide Creation
-#################
-
-import maya.cmds as cmds
+########################################################################################################################
 
 
 def create_head_guides():
     """Creates head guide locators"""
-    # Head Guides
-    cn_head_guide = cmds.spaceLocator(p=(0, 0, 0))
+
+    # Center head guides
+    cn_head_guide = cmds.spaceLocator(p=(0.002, 5.501, 0))
     cmds.rename('locator1', 'cn_head_guide')[0]
 
-    cn_l_eye_guide = cmds.spaceLocator(p=(0, 0, 0))
-    cmds.rename('locator1', 'cn_l_eye_guide')[0]
-
-    cn_r_eye_guide = cmds.spaceLocator(p=(0, 0, 0))
-    cmds.rename('locator1', 'cn_r_eye_guide')[0]
-
-    cn_jaw_guide = cmds.spaceLocator(p=(0, 0, 0))
+    cn_jaw_guide = cmds.spaceLocator(p=(0.004, 5.201, 0.397))
     cmds.rename('locator1', 'cn_jaw_guide')[0]
+
+    # L eye Guide
+    l_eye_guide = cmds.spaceLocator(p=(0.154, 5.749, 0.314))
+    cmds.rename('locator1', 'l_eye_guide')[0]
+
+    # R eye Guide
+    r_eye_guide = cmds.spaceLocator(p=(-0.149, 5.749, 0.314))
+    cmds.rename('locator1', 'r_eye_guide')[0]
 
 
 def create_neck_guides():
     """Creates neck guide locators"""
-    # Neck Guides
-    cn_neck_01_guide = cmds.spaceLocator(p=(0, 0, 0))
+
+    # Neck guides
+    cn_neck_01_guide = cmds.spaceLocator(p=(0, 4.988, 0))
     cmds.rename('locator1', 'cn_neck_01_guide')[0]
 
-    cn_neck_02_guide = cmds.spaceLocator(p=(0, 0, 0))
+    cn_neck_02_guide = cmds.spaceLocator(p=(0.002, 5.164, 0))
     cmds.rename('locator1', 'cn_neck_02_guide')[0]
 
-    cn_neck_03_guide = cmds.spaceLocator(p=(0, 0, 0))
+    cn_neck_03_guide = cmds.spaceLocator(p=(0.002, 5.361, 0))
     cmds.rename('locator1', 'cn_neck_03_guide')[0]
 
 
 def create_torso_guides():
     """Creates torso guide locators"""
-    # Torso Guides
-    cn_spine_01_guide = cmds.spaceLocator(p=(0, 0, 0))
+    # Torso guides
+    cn_spine_01_guide = cmds.spaceLocator(p=(0.002, 3.4, 0.041))
     cmds.rename('locator1', 'cn_spine_01_guide')[0]
 
-    cn_spine_02_guide = cmds.spaceLocator(p=(0, 0, 0))
+    cn_spine_02_guide = cmds.spaceLocator(p=(0.002, 3.603, 0.041))
     cmds.rename('locator1', 'cn_spine_02_guide')[0]
 
-    cn_spine_03_guide = cmds.spaceLocator(p=(0, 0, 0))
+    cn_spine_03_guide = cmds.spaceLocator(p=(0.002, 3.807, 0.041))
     cmds.rename('locator1', 'cn_spine_03_guide')[0]
 
-    cn_spine_04_guide = cmds.spaceLocator(p=(0, 0, 0))
+    cn_spine_04_guide = cmds.spaceLocator(p=(0.002, 4.011, 0.041))
     cmds.rename('locator1', 'cn_spine_04_guide')[0]
 
-    cn_spine_05_guide = cmds.spaceLocator(p=(0, 0, 0))
+    cn_spine_05_guide = cmds.spaceLocator(p=(0.002, 4.215, 0.041))
     cmds.rename('locator1', 'cn_spine_05_guide')[0]
 
-    cn_spine_06_guide = cmds.spaceLocator(p=(0, 0, 0))
+    cn_spine_06_guide = cmds.spaceLocator(p=(0.002, 4.419, 0.041))
     cmds.rename('locator1', 'cn_spine_06_guide')[0]
 
-    cn_spine_07_guide = cmds.spaceLocator(p=(0, 0, 0))
+    cn_spine_07_guide = cmds.spaceLocator(p=(0.002, 4.622, 0.041))
     cmds.rename('locator1', 'cn_spine_07_guide')[0]
 
-    cn_spine_08_guide = cmds.spaceLocator(p=(0, 0, 0))
+    cn_spine_08_guide = cmds.spaceLocator(p=(0.002, 4.826, 0.041))
     cmds.rename('locator1', 'cn_spine_08_guide')[0]
 
 
 def create_arm_guides():
     """Creates arm guide locators"""
-    # L Arm Guides
-    cn_l_shoulder_guide = cmds.spaceLocator(p=(0, 0, 0))
-    cmds.rename('locator1', 'cn_l_shoulder_guide')[0]
+    # L arm guides
+    l_shoulder_guide = cmds.spaceLocator(p=(0.476, 4.901, -0.05))
+    cmds.rename('locator1', 'l_shoulder_guide')[0]
 
-    cn_l_elbow_guide = cmds.spaceLocator(p=(0, 0, 0))
-    cmds.rename('locator1', 'cn_l_elbow_guide')[0]
+    l_clavicle_01_guide = cmds.spaceLocator(p=(0.054, 4.841, 0))
+    cmds.rename('locator1', 'l_clavicle_01_guide')[0]
+
+    l_clavicle_02_guide = cmds.spaceLocator(p=(0.476, 4.901, -0.05))
+    cmds.rename('locator1', 'l_clavicle_02_guide')[0]
+
+    l_elbow_guide = cmds.spaceLocator(p=(1.279, 4.901, -0.049))
+    cmds.rename('locator1', 'l_elbow_guide')[0]
+
+    l_wrist_guide = cmds.spaceLocator(p=(2.144, 4.901, -0.038))
+    cmds.rename('locator1', 'l_wrist_guide')[0]
 
     # R Arm Guides
-    cn_r_shoulder_guide = cmds.spaceLocator(p=(0, 0, 0))
-    cmds.rename('locator1', 'cn_r_shoulder_guide')[0]
+    r_shoulder_guide = cmds.spaceLocator(p=(-0.476, 4.901, -0.05))
+    cmds.rename('locator1', 'r_shoulder_guide')[0]
 
-    cn_r_elbow_guide = cmds.spaceLocator(p=(0, 0, 0))
-    cmds.rename('locator1', 'cn_r_elbow_guide')[0]
+    r_clavicle_01_guide = cmds.spaceLocator(p=(-0.049, 4.841, 0))
+    cmds.rename('locator1', 'r_clavicle_01_guide')[0]
+
+    r_clavicle_02_guide = cmds.spaceLocator(p=(-0.476, 4.901, -0.05))
+    cmds.rename('locator1', 'r_clavicle_02_guide')[0]
+
+    r_shoulder_guide = cmds.spaceLocator(p=(-0.476, 4.901, -0.05))
+    cmds.rename('locator1', 'r_shoulder_guide')[0]
+
+    r_elbow_guide = cmds.spaceLocator(p=(-1.279, 4.901, -0.049))
+    cmds.rename('locator1', 'r_elbow_guide')[0]
+
+    r_wrist_guide = cmds.spaceLocator(p=(-2.144, 4.901, -0.038))
+    cmds.rename('locator1', 'r_wrist_guide')[0]
 
 
 def create_leg_guides():
-    # L Leg Guide
-    cn_l_leg_guide = cmds.spaceLocator(p=(0, 0, 0))
-    cmds.rename('locator1', 'cn_l_leg_guide')[0]
+    """Creates leg guide locators"""
+    # L leg guides
+    l_leg_guide = cmds.spaceLocator(p=(0.183, 3.404, 0.038))
+    cmds.rename('locator1', 'l_leg_guide')[0]
 
-    cn_l_knee_guide = cmds.spaceLocator(p=(0, 0, 0))
-    cmds.rename('locator1', 'cn_l_knee_guide')[0]
+    l_knee_guide = cmds.spaceLocator(p=(0.183, 1.859, 0.064))
+    cmds.rename('locator1', 'l_knee_guide')[0]
 
-    cn_l_ankle_guide = cmds.spaceLocator(p=(0, 0, 0))
-    cmds.rename('locator1', 'cn_l_ankle_guide')[0]
+    l_ankle_guide = cmds.spaceLocator(p=(0.183, 0.182, -0.018))
+    cmds.rename('locator1', 'l_ankle_guide')[0]
 
-    # R Leg Guide
-    cn_r_leg_guide = cmds.spaceLocator(p=(0, 0, 0))
-    cmds.rename('locator1', 'cn_r_leg_guide')[0]
+    l_foot_01_guide = cmds.spaceLocator(p=(-0.183, 0.182, -0.018))
+    cmds.rename('locator1', 'l_foot_01_guide')[0]
 
-    cn_r_knee_guide = cmds.spaceLocator(p=(0, 0, 0))
-    cmds.rename('locator1', 'cn_r_knee_guide')[0]
+    l_foot_02_guide = cmds.spaceLocator(p=(-0.183, 0.182, -0.018))
+    cmds.rename('locator1', 'l_foot_02_guide')[0]
 
-    cn_r_ankle_guide = cmds.spaceLocator(p=(0, 0, 0))
-    cmds.rename('locator1', 'cn_r_ankle_guide')[0]
+    l_foot_03_guide = cmds.spaceLocator(p=(-0.183, 0.182, -0.018))
+    cmds.rename('locator1', 'l_foot_03_guide')[0]
 
+    # R leg guides
+    r_leg_guide = cmds.spaceLocator(p=(-0.183, 3.404, 0.038))
+    cmds.rename('locator1', 'r_leg_guide')[0]
+
+    r_knee_guide = cmds.spaceLocator(p=(-0.183, 1.859, 0.064))
+    cmds.rename('locator1', 'r_knee_guide')[0]
+
+    r_ankle_guide = cmds.spaceLocator(p=(-0.183, 0.182, -0.018))
+    cmds.rename('locator1', 'r_ankle_guide')[0]
+
+    r_foot_01_guide = cmds.spaceLocator(p=(-0.183, 0.182, -0.018))
+    cmds.rename('locator1', 'r_foot_01_guide')[0]
+
+    r_foot_02_guide = cmds.spaceLocator(p=(-0.183, 0.182, -0.018))
+    cmds.rename('locator1', 'r_foot_02_guide')[0]
+
+    r_foot_03_guide = cmds.spaceLocator(p=(-0.183, 0.182, -0.018))
+    cmds.rename('locator1', 'r_foot_03_guide')[0]
 
 def create_hand_guides():
     """Creates hand guide locators"""
+
+    ###################################################################################################
     # L Hand Guides
-    cn_l_index_finger_01_guide = cmds.spaceLocator(p=(0, 0, 0))
-    cmds.rename('locator1', 'cn_l_index_finger_01_guide')[0]
+    ###################################################################################################
 
-    cn_l_index_finger_02_guide = cmds.spaceLocator(p=(0, 0, 0))
-    cmds.rename('locator1', 'cn_l_index_finger_02_guide')[0]
+    # Index
+    l_index_finger_01_guide = cmds.spaceLocator(p=(2.428, 4.913, 0.112))
+    cmds.rename('locator1', 'l_index_finger_01_guide')[0]
 
-    cn_l_index_finger_03_guide = cmds.spaceLocator(p=(0, 0, 0))
-    cmds.rename('locator1', 'cn_l_index_finger_03_guide')[0]
+    l_index_finger_02_guide = cmds.spaceLocator(p=(2.537, 4.913, 0.134))
+    cmds.rename('locator1', 'l_index_finger_02_guide')[0]
 
-    cn_l_ring_finger_01_guide = cmds.spaceLocator(p=(0, 0, 0))
-    cmds.rename('locator1', 'cn_l_ring_finger_01_guide')[0]
+    l_index_finger_03_guide = cmds.spaceLocator(p=(2.669, 4.923, 0.163))
+    cmds.rename('locator1', 'l_index_finger_03_guide')[0]
 
-    cn_l_ring_finger_02_guide = cmds.spaceLocator(p=(0, 0, 0))
-    cmds.rename('locator1', 'cn_l_ring_finger_02_guide')[0]
+    l_index_finger_04_guide = cmds.spaceLocator(p=(2.778, 4.941, 0.181))
+    cmds.rename('locator1', 'l_index_finger_04_guide')[0]
 
-    cn_l_ring_finger_03_guide = cmds.spaceLocator(p=(0, 0, 0))
-    cmds.rename('locator1', 'cn_l_ring_finger_03_guide')[0]
+    # Ring
+    l_ring_finger_01_guide = cmds.spaceLocator(p=(2.442, 4.922, -0.067))
+    cmds.rename('locator1', 'l_ring_finger_01_guide')[0]
 
-    cn_l_middle_finger_01_guide = cmds.spaceLocator(p=(0, 0, 0))
-    cmds.rename('locator1', 'cn_l_middle_finger_01_guide')[0]
+    l_ring_finger_02_guide = cmds.spaceLocator(p=(2.562, 4.929, -0.07))
+    cmds.rename('locator1', 'l_ring_finger_02_guide')[0]
 
-    cn_l_middle_finger_02_guide = cmds.spaceLocator(p=(0, 0, 0))
-    cmds.rename('locator1', 'cn_l_middle_finger_02_guide')[0]
+    l_ring_finger_03_guide = cmds.spaceLocator(p=(2.657, 4.934, -0.072))
+    cmds.rename('locator1', 'l_ring_finger_03_guide')[0]
 
-    cn_l_middle_finger_03_guide = cmds.spaceLocator(p=(0, 0, 0))
-    cmds.rename('locator1', 'cn_l_middle_finger_03_guide')[0]
+    l_ring_finger_04_guide = cmds.spaceLocator(p=(2.807, 4.943, -0.076))
+    cmds.rename('locator1', 'l_ring_finger_04_guide')[0]
 
-    cn_l_pinky_01_guide = cmds.spaceLocator(p=(0, 0, 0))
-    cmds.rename('locator1', 'cn_l_pinky_01_guide')[0]
+    # Middle
+    l_middle_finger_01_guide = cmds.spaceLocator(p=(2.444, 4.923, 0.025))
+    cmds.rename('locator1', 'l_middle_finger_01_guide')[0]
 
-    cn_l_pinky_02_guide = cmds.spaceLocator(p=(0, 0, 0))
-    cmds.rename('locator1', 'cn_l_pinky_02_guide')[0]
+    l_middle_finger_02_guide = cmds.spaceLocator(p=(2.567, 4.921, 0.032))
+    cmds.rename('locator1', 'l_middle_finger_02_guide')[0]
 
-    cn_l_pinky_03_guide = cmds.spaceLocator(p=(0, 0, 0))
-    cmds.rename('locator1', 'cn_l_pinky_03_guide')[0]
+    l_middle_finger_03_guide = cmds.spaceLocator(p=(2.691, 4.927, 0.038))
+    cmds.rename('locator1', 'l_middle_finger_03_guide')[0]
 
-    cn_l_thumb_01_guide = cmds.spaceLocator(p=(0, 0, 0))
-    cmds.rename('locator1', 'cn_l_thumb_01_guide')[0]
+    l_middle_finger_04_guide = cmds.spaceLocator(p=(2.835, 4.964, 0.047))
+    cmds.rename('locator1', 'l_middle_finger_04_guide')[0]
 
-    cn_l_thumb_02_guide = cmds.spaceLocator(p=(0, 0, 0))
-    cmds.rename('locator1', 'cn_l_thumb_02_guide')[0]
+    # Pinky
+    l_pinky_01_guide = cmds.spaceLocator(p=(2.413, 4.91, -0.148))
+    cmds.rename('locator1', 'l_pinky_01_guide')[0]
 
-    cn_l_thumb_03_guide = cmds.spaceLocator(p=(0, 0, 0))
-    cmds.rename('locator1', 'cn_l_thumb_03_guide')[0]
+    l_pinky_02_guide = cmds.spaceLocator(p=(2.517, 4.906, -0.165))
+    cmds.rename('locator1', 'l_pinky_02_guide')[0]
 
-    cn_l_thumb_04_guide = cmds.spaceLocator(p=(0, 0, 0))
-    cmds.rename('locator1', 'cn_l_thumb_04_guide')[0]
+    l_pinky_03_guide = cmds.spaceLocator(p=(2.597, 4.904, -0.176))
+    cmds.rename('locator1', 'l_pinky_03_guide')[0]
 
+    l_pinky_04_guide = cmds.spaceLocator(p=(2.702, 4.928, -0.187))
+    cmds.rename('locator1', 'l_pinky_04_guide')[0]
+
+    # Thumb
+    l_thumb_01_guide = cmds.spaceLocator(p=(2.221, 4.879, 0.092))
+    cmds.rename('locator1', 'l_thumb_01_guide')[0]
+
+    l_thumb_02_guide = cmds.spaceLocator(p=(2.27, 4.828, 0.18))
+    cmds.rename('locator1', 'l_thumb_02_guide')[0]
+
+    l_thumb_03_guide = cmds.spaceLocator(p=(2.319, 4.784, 0.254))
+    cmds.rename('locator1', 'l_thumb_03_guide')[0]
+
+    l_thumb_04_guide = cmds.spaceLocator(p=(2.376, 4.745, 0.368))
+    cmds.rename('locator1', 'l_thumb_04_guide')[0]
+
+    ##################################################################################################
     # R Hand Guides
-    cn_r_index_finger_01_guide = cmds.spaceLocator(p=(0, 0, 0))
-    cmds.rename('locator1', 'cn_r_index_finger_01_guide')[0]
+    ##################################################################################################
 
-    cn_r_index_finger_02_guide = cmds.spaceLocator(p=(0, 0, 0))
-    cmds.rename('locator1', 'cn_r_index_finger_02_guide')[0]
+    # Index
+    r_index_finger_01_guide = cmds.spaceLocator(p=(-2.428, 4.913, 0.112))
+    cmds.rename('locator1', 'r_index_finger_01_guide')[0]
 
-    cn_r_index_finger_03_guide = cmds.spaceLocator(p=(0, 0, 0))
-    cmds.rename('locator1', 'cn_r_index_finger_03_guide')[0]
+    r_index_finger_02_guide = cmds.spaceLocator(p=(-2.537, 4.913, 0.134))
+    cmds.rename('locator1', 'r_index_finger_02_guide')[0]
 
-    cn_r_ring_finger_01_guide = cmds.spaceLocator(p=(0, 0, 0))
-    cmds.rename('locator1', 'cn_r_ring_finger_01_guide')[0]
+    r_index_finger_03_guide = cmds.spaceLocator(p=(-2.669, 4.923, 0.163))
+    cmds.rename('locator1', 'r_index_finger_03_guide')[0]
 
-    cn_r_ring_finger_02_guide = cmds.spaceLocator(p=(0, 0, 0))
-    cmds.rename('locator1', 'cn_r_ring_finger_02_guide')[0]
+    r_index_finger_04_guide = cmds.spaceLocator(p=(-2.778, 4.941, 0.181))
+    cmds.rename('locator1', 'r_index_finger_04_guide')[0]
 
-    cn_r_ring_finger_03_guide = cmds.spaceLocator(p=(0, 0, 0))
-    cmds.rename('locator1', 'cn_r_ring_finger_03_guide')[0]
+    # Ring
+    r_ring_finger_01_guide = cmds.spaceLocator(p=(-2.442, 4.922, -0.067))
+    cmds.rename('locator1', 'r_ring_finger_01_guide')[0]
 
-    cn_r_middle_finger_01_guide = cmds.spaceLocator(p=(0, 0, 0))
-    cmds.rename('locator1', 'cn_r_middle_finger_01_guide')[0]
+    r_ring_finger_02_guide = cmds.spaceLocator(p=(-2.562, 4.929, -0.07))
+    cmds.rename('locator1', 'r_ring_finger_02_guide')[0]
 
-    cn_r_middle_finger_02_guide = cmds.spaceLocator(p=(0, 0, 0))
-    cmds.rename('locator1', 'cn_r_middle_finger_02_guide')[0]
+    r_ring_finger_03_guide = cmds.spaceLocator(p=(-2.657, 4.929, -0.075))
+    cmds.rename('locator1', 'r_ring_finger_03_guide')[0]
 
-    cn_r_middle_finger_03_guide = cmds.spaceLocator(p=(0, 0, 0))
-    cmds.rename('locator1', 'cn_r_middle_finger_03_guide')[0]
+    r_ring_finger_04_guide = cmds.spaceLocator(p=(-2.805, 4.954, -0.079))
+    cmds.rename('locator1', 'r_ring_finger_04_guide')[0]
 
-    cn_r_pinky_01_guide = cmds.spaceLocator(p=(0, 0, 0))
-    cmds.rename('locator1', 'cn_r_pinky_01_guide')[0]
+    # Middle
+    r_middle_finger_01_guide = cmds.spaceLocator(p=(-2.444, 4.923, 0.025))
+    cmds.rename('locator1', 'r_middle_finger_01_guide')[0]
 
-    cn_r_pinky_02_guide = cmds.spaceLocator(p=(0, 0, 0))
-    cmds.rename('locator1', 'cn_r_pinky_02_guide')[0]
+    r_middle_finger_02_guide = cmds.spaceLocator(p=(-2.568, 4.921, 0.032))
+    cmds.rename('locator1', 'r_middle_finger_02_guide')[0]
 
-    cn_r_pinky_03_guide = cmds.spaceLocator(p=(0, 0, 0))
-    cmds.rename('locator1', 'cn_r_pinky_03_guide')[0]
+    r_middle_finger_03_guide = cmds.spaceLocator(p=(-2.691, 4.927, 0.038))
+    cmds.rename('locator1', 'r_middle_finger_03_guide')[0]
 
-    cn_r_thumb_01_guide = cmds.spaceLocator(p=(0, 0, 0))
-    cmds.rename('locator1', 'cn_r_thumb_01_guide')[0]
+    r_middle_finger_04_guide = cmds.spaceLocator(p=(-2.835, 4.964, 0.047))
+    cmds.rename('locator1', 'r_middle_finger_04_guide')[0]
 
-    cn_r_thumb_02_guide = cmds.spaceLocator(p=(0, 0, 0))
-    cmds.rename('locator1', 'cn_r_thumb_02_guide')[0]
+    # Pinky
+    r_pinky_01_guide = cmds.spaceLocator(p=(-2.413, 4.91, -0.148))
+    cmds.rename('locator1', 'r_pinky_01_guide')[0]
 
-    cn_r_thumb_03_guide = cmds.spaceLocator(p=(0, 0, 0))
-    cmds.rename('locator1', 'cn_r_thumb_03_guide')[0]
+    r_pinky_02_guide = cmds.spaceLocator(p=(-2.517, 4.906, -0.165))
+    cmds.rename('locator1', 'r_pinky_02_guide')[0]
 
-    cn_r_thumb_04_guide = cmds.spaceLocator(p=(0, 0, 0))
-    cmds.rename('locator1', 'cn_r_thumb_04_guide')[0]
+    r_pinky_03_guide = cmds.spaceLocator(p=(-2.597, 4.904, -0.176))
+    cmds.rename('locator1', 'r_pinky_03_guide')[0]
+
+    r_pinky_04_guide = cmds.spaceLocator(p=(-2.702, 4.928, -0.187))
+    cmds.rename('locator1', 'r_pinky_04_guide')[0]
+
+    # Thumb
+    r_thumb_01_guide = cmds.spaceLocator(p=(-2.702, 4.879, 0.092))
+    cmds.rename('locator1', 'r_thumb_01_guide')[0]
+
+    r_thumb_02_guide = cmds.spaceLocator(p=(-2.27, 4.828, 0.18))
+    cmds.rename('locator1', 'r_thumb_02_guide')[0]
+
+    r_thumb_03_guide = cmds.spaceLocator(p=(-2.319, 4.784, 0.254))
+    cmds.rename('locator1', 'r_thumb_03_guide')[0]
+
+    r_thumb_04_guide = cmds.spaceLocator(p=(-2.376, 4.745, 0.368))
+    cmds.rename('locator1', 'r_thumb_04_guide')[0]
 
 
 def create_all_guides():
-    """Create all biped rig placement guides."""
+    """Creates all biped rig placement guides."""
     create_head_guides()
     create_neck_guides()
     create_torso_guides()
@@ -223,5 +307,43 @@ def create_all_guides():
     create_leg_guides()
     create_hand_guides()
 
+
+########################################################################################################################
+# Skeleton Creation
+########################################################################################################################
+def create_head_joints():
+    """Builds skeleton at the location of guide locators."""
+
+def create_neck_joints():
+    """Builds skeleton at the location of guide locators."""
+
+def create_torso_joints():
+    """Builds skeleton at the location of guide locators."""
+
+def create_arm_joints():
+    """Builds skeleton at the location of guide locators."""
+
+def create_leg_joints():
+    """Builds skeleton at the location of guide locators."""
+
+def create_hand_joints():
+    """Builds skeleton at the location of guide locators."""
+    
+    # R Hand
+    
+    # Index 
+    cmds.matchTransform('')
+    hands = cmds.joint(p=(0, 0, 0))
+
 def build_skeleton():
-    """ Builds skeleton at the location of guide locators"""
+    """Builds skeleton at the location of guide locators."""
+    create_head_joints()
+    create_neck_joints()
+    create_torso_joints()
+    create_arm_joints()
+    create_leg_joints()
+    create_hand_joints()
+
+create_all_guides()
+
+build_skeleton()
